634d3887a72b14f6715f310730a879f0b5d2b7ae
Valerio Leo <mail.valerioleo@gmail.com>
2020-01-10 10:45:24 +0000

    getTransactionData :: it is now compatible with deploy txs

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// As we don't use webpack in the common-ui folder,
+// we can use "babel-module"
+
+module.exports = {
+  rules: {
+    'no-use-before-define': [0],
+    'no-restricted-properties': [0],
+    'consistent-return': [0],
+    'no-shadow': [0],
+    'no-bitwise': [0],
+    'no-console': [0],
+    'no-prototype-builtins': [0],
+    'no-param-reassign': [0],
+    'max-classes-per-file': [0],
+    'camelcase': [0],
+    'no-underscore-dangle': [0]
+  }
+}
