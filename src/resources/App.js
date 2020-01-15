84650530e128273b9beb1c52b60a743d8d22ebc0
Valerio Leo <mail.valerioleo@gmail.com>
2020-01-15 18:33:44 +0000

    sdk :: improved createApp function

--- src/resources/App.js
+++ src/resources/App.js
@@ -1,28 +1,46 @@
-const createAppHandler = (wsClient, coincierge, signer) => async params => {
+/* eslint-disable no-case-declarations */
+const createAppHandler = (
+  wsClient,
+  coincierge,
+  signer
+) => async params => new Promise((res, rej) => {
   wsClient.subscribe(async ws => {
     ws.send(JSON.stringify(params));
 
     ws.on('message', async message => {
       const {type, data} = JSON.parse(message);
 
-      if(type === 'signable_tx') {
-        const {
-          nonce,
-          to,
-          value,
-          inputData,
-          gasLimit,
-          gasPrice,
-          id: txId
-        } = data;
+      switch(type) {
+        case 'signable_tx':
+          const {
+            nonce,
+            to,
+            value,
+            inputData,
+            gasLimit,
+            gasPrice,
+            id: txId,
+            appId
+          } = data;
 
-        const signedTx = await signer(nonce, to, value, inputData, gasLimit, gasPrice);
+          const signedTx = await signer(nonce, to, value, inputData, gasLimit, gasPrice);
 
-        await coincierge.transactions.finalize({signedTx: signedTx.toString('hex')}, {txId, appId: params.appId});
+          await coincierge.transactions.finalize({signedTx: signedTx.toString('hex')}, {txId, appId});
+          break;
+
+        case 'complete':
+          res(data);
+          ws.close();
+          break;
+
+        default:
+          console.log(data);
+          break;
       }
     });
+    ws.on('error', rej);
   });
-};
+});
 
 const appResource = (httpClient, wsAgent, coincierge, signer) => {
   const basePath = 'apps';
