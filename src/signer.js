3ceed006adae68533c7a45f41d18d241b513c1c9
Valerio Leo <mail.valerioleo@gmail.com>
2020-01-29 21:57:52 +0000

    sdk :: better error message for default signer

--- src/signer.js
+++ src/signer.js
@@ -3,11 +3,19 @@ const ethers = require('ethers');
 const isMnemonin = words => words.split(' ').length > 1;
 
 const signer = privKey => {
-  const wallet = isMnemonin(privKey)
-    ? ethers.Wallet.fromMnemonic(privKey)
-    : new ethers.Wallet(privKey);
+  try {
+    if(!privKey || typeof privKey !== 'string') {
+      throw Error('To use the default signer you must pass a valid privKey string. Both hex or mnemonic versions are accepted.');
+    }
+    const wallet = isMnemonin(privKey)
+      ? ethers.Wallet.fromMnemonic(privKey)
+      : new ethers.Wallet(privKey);
 
-  return wallet.sign;
+    return wallet.sign;
+  }
+  catch(error) {
+    throw Error(`Impossible initialise default signer: ${error.message}`);
+  }
 };
 
 module.exports = {
