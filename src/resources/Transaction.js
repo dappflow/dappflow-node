8b4c7b8c1189a7c92149b22b5e1e99d89741fe4a
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2020-03-19 19:52:49 +0200

    added transactions.list

--- src/resources/Transaction.js
+++ src/resources/Transaction.js
@@ -1,10 +1,15 @@
-const transactionResource = ({wsAgent}) => {
+const transactionResource = ({wsAgent, httpAgent}) => {
   const basePath = 'transactions';
 
   const transactions = {
     finalize: wsAgent({
       method: 'GET',
       path: `/apps/{appId}/${basePath}/{txId}`
+    }),
+
+    list: httpAgent({
+      method: 'GET',
+      path: '/apps/{appId}/transactions'
     })
   };
 
