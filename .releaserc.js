edc52c63d815f7b7ed016c98bfe6e440eeb2d294
Valerio Leo <mail.valerioleo@gmail.com>
2020-04-27 12:16:31 +0100

    ci: added semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  branches: ['fix-web3-changes']
+  branches: ['fix-web3-changes'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm'
+  ]
 }
