e921794ddc5d8bdb1bf60911a68e57278a7021cf
Valerio Leo <mail.valerioleo@gmail.com>
2020-04-27 15:20:30 +0100

    fix: added changelog + git release plugin

--- .releaserc.js
+++ .releaserc.js
@@ -3,11 +3,18 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/npm", {
-      "npmPublish": true
+    ['@semantic-release/npm', {
+      'npmPublish': true
     }],
-    ["@semantic-release/github", {
-      "publish": true
+    ['@semantic-release/github', {
+      'publish': true
+    }],
+    ['@semantic-release/git', {
+      'assets': ['CHANGELOG.md', 'package.json'],
+      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    ['@semantic-release/changelog', {
+      'changelogFile': 'CHANGELOG.md',
     }]
   ]
 }
